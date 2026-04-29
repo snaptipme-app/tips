25c2d34df86e19ffcdf17972475b52d8f96ad66d
SnapTip <snaptip.me@gmail.com>
2026-04-29 21:00:30 +0100

    fix: babel config cache conflict

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,14 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
-  const isProd = api.env('production');
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      // Strip console.* in production builds, but keep error/warn so we
-      // still surface real problems via crash reporting.
-      ...(isProd
-        ? [['transform-remove-console', { exclude: ['error', 'warn'] }]]
-        : []),
-    ],
+    plugins: [],
   };
 };
